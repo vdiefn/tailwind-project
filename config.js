d20cba5ca45071826f34d2c4f754f8737a3c444f
Ellen <ddhkxxhxx@hotmail.com>
2025-03-09 22:17:36 +0800

    feat: add font-famliy and font-size

---
+++ config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        pale_orange: "#ffd9a6",
+        light_orange: "#fbb03b",
+        orange: "#f7931e"
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        title: ["Playfair Display SC", "serif"]
+      }
+    }
+  }
+}
